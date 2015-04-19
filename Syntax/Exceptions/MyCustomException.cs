3c72dc0628fbd9ba9bbf96db6249e416e856ebcc
anamariam <anamunte898@yahoo.com>
2015-04-19 15:06:22 +0300

    Exceptions

---
+++ Syntax/Exceptions/MyCustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class MyCustomException : Exception
+    {
+        public MyCustomException()
+        {
+        }
+
+        public MyCustomException(string message)
+            : base(message)
+        {
+        }
+        public MyCustomException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Syntax/Exceptions/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void CatchException(string tellMeSomething)
+         {
+            try
+            {
+                var length = tellMeSomething.Length;
+
+                Console.WriteLine(length);
+
+                bool isThisBoolean = Boolean.Parse(tellMeSomething);
+
+                Console.WriteLine(isThisBoolean);
+            }
+            
+             catch (NullReferenceException)
+             {
+               
+                 Console.WriteLine("Oooops!!Null");
+             }
+            catch (FormatException)
+            {
+                Console.WriteLine("Oooops!!Parsing failed");
+            }
+             finally
+             {
+                 Console.WriteLine("I really don't care what you do, I always write this!!:)");
+             }
+
+         }
+
+        private static void FileExeption()
+
+        {
+            System.IO.FileStream file = null;
+            System.IO.FileInfo fileinfo = new System.IO.FileInfo("D:\\file.txt");
+            try
+            {
+                file = fileinfo.OpenWrite();
+                file.WriteByte(0xF);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var x = Console.ReadLine();
+
+            if (x.StartsWith("a"))
+            {
+                throw new MyCustomException("Well, something went wrong");
+            }
+            //CatchException(x);
+            //CatchException(null);
+            //FileExeption();
+            Console.ReadLine();
+        }
+    }
+}
