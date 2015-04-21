375d4cc8453ae907bc8603daab45e6cf2cfaa1db
Ana Predescu <anitzapredescu@gmail.com>
2015-04-21 14:32:36 +0300

    Added examples for Threads project

---
+++ Syntax/Threads/TasksTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    public class TasksTest
+    {
+        public static void Execute()
+        {
+            //var newTask = Task.Run(() => Go());
+            var newTask = Task.Factory.StartNew(Go);
+            newTask.Wait();
+
+            Console.ReadKey();
+
+            // Start the task executing:
+            Task<string> task = Task.Factory.StartNew<string>
+                (() => DownloadString("http://www.linqpad.net"));
+
+            // We can do other work here and it will execute in parallel:
+            RunSomeOtherMethod();
+
+            // When we need the task's return value, we query its Result property:
+            // If it's still executing, the current thread will now block (wait)
+            // until the task finishes:
+            string result = task.Result;
+            
+            
+            Console.ReadKey();
+        }
+
+        private static void RunSomeOtherMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static string DownloadString(string uri)
+        {
+            using (var wc = new System.Net.WebClient())
+                return wc.DownloadString(uri);
+        }
+
+        static void Go()
+        {
+            Console.WriteLine("Hello from the thread pool!");
+        }
+    }
+}
---
+++ Syntax/Threads/ThreadPoolTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    class ThreadPoolTest
+    {
+        public static void Execute()
+        {
+            WaitCallback callBack;
+            // Main method
+            Console.WriteLine("Main thread. Is pool thread: {0}, Hash: {1}",
+                     Thread.CurrentThread.IsThreadPoolThread,
+                     Thread.CurrentThread.GetHashCode());
+
+            callBack = new WaitCallback(PooledFunc);
+            ThreadPool.QueueUserWorkItem(callBack,
+               "Is there any screw left?");
+            ThreadPool.QueueUserWorkItem(callBack,
+               "How much is a 40W bulb?");
+            ThreadPool.QueueUserWorkItem(callBack,
+               "Decrease stock of monkey wrench");
+            Console.ReadLine();
+        }
+
+        static void PooledFunc(object state)
+        {
+            // Pool method
+            Console.WriteLine("Processing request '{0}'." +
+               " Is pool thread: {1}, Hash: {2}",
+               (string)state, Thread.CurrentThread.IsThreadPoolThread,
+               Thread.CurrentThread.GetHashCode());
+
+            // Simulation of processing time
+            Thread.Sleep(2000);
+            Console.WriteLine("Request processed");
+        }
+    }
+}
